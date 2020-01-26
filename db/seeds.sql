07726fba13d9dbbe3c4c7a6dff08a453783ab355
JGen1 <jengenes01@gmail.com>
2020-01-26 00:21:42 -0500

    Init config,db,and server.js

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+USE burger_db;
+INSERT INTO burgers (burger_name,devoured) VALUES ("Cheeseburger",false),("Chili burger",false),("Hamburger",false),("Veggie burger",false),("Bacon cheeseburger",true),("Honker burger",false),("Turkey burger",false),("Tofu burger",true);
