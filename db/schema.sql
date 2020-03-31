423bf5fc7275ea8a6d3c207fff84a2e65f6fe94d
JGen1 <jengenes01@gmail.com>
2020-03-31 14:58:02 -0400

    Changed DB to delete before create.

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,12 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
-DROP TABLE IF EXISTS `burgers`;
-CREATE TABLE `burgers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `burger_name` varchar(50) DEFAULT NULL,
-  `devoured` tinyint(1) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) 
+
+-- Create the table burgers.
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
