5d5207bd63b5d395a138467258ba67bbf9bc9f0f
JGen1 <jengenes01@gmail.com>
2020-03-31 16:26:10 -0400

    Fixed connection.js db info, fixed formatting and removed commented out info

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Biodesi647025!",
-  database: "burger_db"
+  database: "burgers_db"
 });
 
 // Make connection.
@@ -20,26 +19,4 @@ connection.connect(function(err) {
 
 // Export connection.
 module.exports = connection;
-var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "burgers_db"
-    });
-};
-
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-// Export the connection.
-module.exports = connection;
